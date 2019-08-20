39c702cd449cc8548f2f3d06aa304fcfba3b542f
Hic <hic.hooria@gmail.com>
2019-08-20 15:15:11 +0500

    removed extra spacings

--- Routes.js
+++ Routes.js
@@ -1,5 +1,5 @@
-const passport    = require('passport');
-const bcrypt      = require('bcrypt');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
 
 module.exports = function (app, db) {
       
